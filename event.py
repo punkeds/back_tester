447bdc3b81ab8dc835cf4373ceb80ea38d3588d8
punkeds <alecspraslow@gmail.com>
2015-10-28 16:18:45 +0300

    add event

---
+++ event.py
@@ -0,0 +1,28 @@
+class Event(object):
+    pass
+
+
+class MarketEvent(Event):
+
+    def __init__(self):
+        self.type = 'MARKET'
+
+
+class SignalEvent():
+
+    #Обрабатывает событие отправки Signal из объекта Strategy. Его получает объект Portfolio, который предпринимает нужное действие.
+
+
+    def __init__(self, symbol, datetime, signal_type):
+
+        ''' Инициализирует SignalEvent.
+
+        Параметры:
+        symbol - Символ тикера, например для Google — 'GOOG'.
+        datetime - временная метка момента генерации сигнала.
+        signal_type - 'LONG' или 'SHORT' '''
+
+        self.type = 'SIGNAL'
+        self.symbol = symbol
+        self.datetime = datetime
+        self.signal_type = signal_type
